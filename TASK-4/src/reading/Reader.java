6c014e2ea092ef6fcc74753895704d03d863a36c
stibel <mikisiebielec@gmail.com>
2021-06-13 15:21:27 +0200

    Task eight finished

--- TASK-4/src/reading/Reader.java
+++ TASK-4/src/reading/Reader.java
@@ -1,6 +1,14 @@
 package reading;
 
+import java.util.Vector;
+import java.util.concurrent.BlockingQueue;
+
 public class Reader {
     private int id;
     private int wantedBookId;
+
+    public Reader(BlockingQueue<Integer> queue, Vector<Integer> books, Vector<Integer> seats) {
+
+    }
+
 }
--- TASK-4/src/reading/ReadingRoom.java
+++ TASK-4/src/reading/ReadingRoom.java
@@ -1,17 +1,23 @@
 package reading;
 
-import java.util.ArrayList;
+import java.util.Vector;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.TimerTask;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executors;
 
 public class ReadingRoom extends TimerTask {
-    private final int books;
-    private final int seats;
+//    private final int books;
+//    private final int seats;
     private final int readersInterval;
     private final int readingTime;
-    private LinkedList<Reader> queue;
-    private ArrayList<Integer> unavailableBooks;
+//    private LinkedList<Reader> queue;
+    private Vector<Integer> books;
+    private Vector<Integer> seats;
+    private Vector<Integer> reservedBooks;
+    private Vector<Integer> reservedSeats;
+    private BlockingQueue<Integer> queue;
 
     private int getRandom(int min, int max) {
         Random rand = new Random();
@@ -19,10 +25,23 @@ public class ReadingRoom extends TimerTask {
     }
 
     public ReadingRoom(int min, int max) {
-        this.books = getRandom(min, max);
-        this.seats = getRandom(min, max);
+        var booksNum = getRandom(min, max);
+
+        for(var i = 0; i < booksNum; ++i)
+            books.add(i);
+
+        var seatsNum = getRandom(min, max);
+        for(var i = 0; i < seatsNum; ++i)
+            seats.add(i);
+
         this.readersInterval = getRandom(min, max);
         this.readingTime = getRandom(min, max);
+    }
+
+    public void start() {
+        while (true) {
+            
+        }
     }
 
     @Override
